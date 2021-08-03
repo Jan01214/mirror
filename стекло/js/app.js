e709841fe24f0ba3870602080da563b1eea3a22a
Jan01214 <71278204+Jan01214@users.noreply.github.com>
2021-08-03 21:08:29 +0300

    i dont know
    Add link and 3 blocks

--- стекло/js/app.js
+++ стекло/js/app.js
@@ -1,7 +1,39 @@
-function animateBurger(){
-	var burger = document.querySelector('burger');
-	alert('burger')
-	// burger.add.eventListener{('click')
-	// 	burger.classList.toggle('active');
-	}	
-}
+const swiper = new Swiper('.swiper-container', {
+	// Optional parameters
+	direction: 'horizontal',
+	loop: true,
+  
+	// If we need pagination
+	pagination: {
+	  el: '.swiper-pagination',
+	},
+  
+	// Navigation arrows
+	navigation: {
+	  nextEl: '.swiper-button-next',
+	  prevEl: '.swiper-button-prev',
+	},
+  
+	// And if we need scrollbar
+	scrollbar: {
+	  el: '.swiper-scrollbar',
+	},
+  });
+
+  var galleryThumbs = new Swiper('.gallery-thumbs', {
+	spaceBetween: 10,
+	slidesPerView: 1,
+	freeMode: true,
+	watchSlidesVisibility: true,
+	watchSlidesProgress: true,
+  });
+  var galleryTop = new Swiper('.gallery-top', {
+	spaceBetween: 10,
+	navigation: {
+	  nextEl: '.swiper-button-next',
+	  prevEl: '.swiper-button-prev',
+	},
+	thumbs: {
+	  swiper: galleryThumbs
+	}
+  });
